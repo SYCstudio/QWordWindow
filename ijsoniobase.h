c0196c6dc1c43149c7a17e6cac057c277a9097b6
sycstudio <1726016246@qq.com>
2021-09-17 23:13:58 +0800

    :bookmark:  complete JsonIOBase Interface

--- ijsoniobase.h
+++ ijsoniobase.h
@@ -1,11 +1,18 @@
+/*
+定义与Json交互的基础接口
+Date: 2021.9.17
+Author: syc
+*/
 #ifndef IJSONIOBASE_H
 #define IJSONIOBASE_H
-
+#include <QJsonArray>
 
 class IJsonIOBase
 {
 public:
     IJsonIOBase();
+    virtual void parseFromJson(QJsonArray mJarray);
+    virtual QJsonArray parseToJson();
 };
 
 #endif // IJSONIOBASE_H
